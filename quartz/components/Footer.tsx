2eecdd4c58609b0e54c712fb0182818e45b532d7
skydtrtzmr <2368178395@qq.com>
2026-01-08 20:40:50 +0800

    初始化：公司内容定制调整

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,8 +14,9 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          {/* {i18n(cfg.locale).components.footer.createdWith}{" "}
+          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> */}
+          南京源悦 © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
