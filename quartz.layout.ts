91dea3995dfae9fba44415356600edf62ee81b0d
skydtrtzmr <2368178395@qq.com>
2026-01-12 18:31:50 +0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [
-    // Component.CasdoorUserinfo(), // 显示用户名
+    Component.CasdoorUserinfo(), // 显示用户名
     Component.CasdoorLogout(),   // 显示退出按钮
   ],
   afterBody: [],
