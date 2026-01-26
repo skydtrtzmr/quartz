7eb36047e8d287b38c908516497447709baf6cfa
skydtrtzmr <2368178395@qq.com>
2026-01-26 15:21:47 +0800

    Squashed commit of the following: Localizing assets
    资源本地化处理

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -50,7 +50,8 @@ export default (() => {
             )}
           </>
         )}
-        <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
+        {/* 去掉CDN，使用本地资源 */}
+        {/* <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" /> */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
